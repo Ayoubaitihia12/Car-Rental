e14a1b281148dd88395d6326a1c981e050b5fe4a
ayoub <ayoubaitihia@gmail.com>
2023-11-12 00:40:18 +0100

    V.12.11.23.00.40

--- Frontend/src/pages/public/Home.jsx
+++ Frontend/src/pages/public/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import {BsArrowRight} from "react-icons/bs"
 
 const Home = () => {
   return (
     <div className='max-w-6xl w-full mx-auto px-6 py-20 lg:px-8 grid grid-cols-2'>
       
       <div className='flex flex-col gap-y-3'>
-        <h4>Car Rental Service</h4>
-        <h1 className='text-5xl font-bold'>The Best Quality Car's With Us</h1>
+        <div className='flex text-lg items-center gap-x-2'>
+          Car Rental Service
+          <BsArrowRight />
+        </div>
+        <h1 className='text-5xl font-bold'>The Best Quality Car's With Us.</h1>
         <p className='text-sm'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Quod, cupiditate! Doloribus error maxime ipsum vel? Eos
