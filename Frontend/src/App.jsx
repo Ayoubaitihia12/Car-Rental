01a8dc07f0d36c2b17f83eca7013d9f8e6abfe3d
ayoub <ayoubaitihia@gmail.com>
2023-11-14 04:01:44 +0100

    V.14.11.23.04.01

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,9 +4,9 @@ import { RouterProvider } from "react-router-dom"
 
 function App() {
   return (
-    <>
+    <div className='scrollbar scrollbar-thumb-gray-900 scrollbar-track-gray-100'>
       <RouterProvider router={router} />
-    </>
+    </div>
   )
 }
 
